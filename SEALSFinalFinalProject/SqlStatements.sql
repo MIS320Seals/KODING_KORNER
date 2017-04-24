d29e97091ae0a746547f7598551423ec999d1207
sidneybomeke <smbomeke@crimson.ua.edu>
2017-04-23 22:03:00 -0500

    changes added to SQL file

--- SEALSFinalFinalProject/SqlStatements.sql
+++ SEALSFinalFinalProject/SqlStatements.sql
@@ -29,6 +29,12 @@ update sakila.customer
 set password = 'hi'
 where customer_id = 1;
 
+UPDATE sakila.staff SET `first_name` = 'Ta', `last_name` = 'Ta', `email` = 'TA@sakilastaff.com', `username` = 'TA', `password` = 'TA' WHERE staff_id = 1;
+
+Alter table sakila.address delete column location
+
+Alter table sakila.staff delete column picture
+
 create Table cart(
 cart_id int NOT NULL AUTO_INCREMENT,
 customer_id SMALLINT,
